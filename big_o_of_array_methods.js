f748671bd9837507ce0763f1ed331a376c36fcc2
Loren Rachelle Hicks <lorenhicks@gmail.com>
2020-10-23 04:30:40 -0400

    Big O of Array Methods

---
+++ big_o_of_array_methods.js
@@ -0,0 +1,20 @@
+// Insertion - dependent
+// Removal - dependent
+// Searching - O(N)
+// Access - O(1)
+
+// Push and Pop - often faster than Shift and Unshift
+// Altering array at beginning often increases n time due to needing to reindex all elements in array
+// Best practice if possible alter array at end
+
+//Big O of Array Operations
+
+// push - O(1)
+// pop - O(1)
+// shift - O(N)
+// unshift - O(N)
+// concat - O(N)
+// slice - O(N)
+// splice - O(N)
+// sort - O(N * log N)
+// forEach/map/filter/reduce/etc. - O(N)
