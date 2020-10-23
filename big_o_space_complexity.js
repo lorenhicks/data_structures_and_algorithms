93258bfb94efa6750f0f9c76e4ee90c2481bd11d
Loren Rachelle Hicks <lorenhicks@gmail.com>
2020-10-23 03:12:31 -0400

    Big O Space Complexity

---
+++ big_o_space_complexity.js
@@ -0,0 +1,21 @@
+// auxiliary space complexity - space required by the algorithm sans input
+
+// O(1) Space Complexity
+
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+// O(n) Space Complexity
+
+function double (arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(2 * arr[i]);
+    }
+    return newArr;
+}
