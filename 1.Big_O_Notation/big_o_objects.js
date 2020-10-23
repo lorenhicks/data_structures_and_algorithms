ea5a01d2532cf84f9fe3e449f35a1b9d3804c03c
Loren Rachelle Hicks <lorenhicks@gmail.com>
2020-10-23 04:01:03 -0400

    Big O of Objects

---
+++ 1.Big_O_Notation/big_o_objects.js
@@ -0,0 +1,21 @@
+// Big O of Objects
+
+// Insertion - O(1)
+// Removal - O(1)
+// Searching - O(n)
+// Access - O(1)
+
+let instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1,2,3,4]
+}
+
+
+// Big O of Object Methods
+
+// Object.keys - O(N)
+// Object.values - O(N)
+// Object.entries - O(N)
+// hasOwnProperty - O(1)
+
