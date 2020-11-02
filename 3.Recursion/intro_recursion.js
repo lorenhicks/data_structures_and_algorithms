6522d97d653da35dbd2d8adee2f60d5c6fc7b501
Loren Rachelle Hicks <lorenhicks@gmail.com>
2020-11-02 18:41:50 -0500

    Recursion

---
+++ 3.Recursion/intro_recursion.js
@@ -0,0 +1,6 @@
+// RECURSION INTRO
+
+// Define what recursion is and how it can be used
+// Understand the two essential components of a recursive function
+// Visualize the call stack to better debug and understand recursive functions
+// Use helper method recursion and pure recursion to solve more difficult problems
